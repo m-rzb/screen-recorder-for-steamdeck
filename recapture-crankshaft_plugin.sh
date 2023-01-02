27ce8ea93a238040a290f71ae0d5ba6ae5c1a781
m-rzb <43208506+m-rzb@users.noreply.github.com>
2023-01-02 20:16:38 +0000

    Add files via upload

---
+++ recapture-crankshaft_plugin.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Usage: recapture (COMMAND) [OPTIONS...]
+# 
+# Where COMMAND is one of:
+#   record
+#         Start recording your screen, then save the recording when this program
+#         exits.
+#   replay - NOT WORKING
+#         Start recording your screen, then save the most recent X seconds when
+#         this program exits, where X is set by the '-length' option.
+# 
+# Available OPTIONS:
+#   -audio-bitrate string
+#         Audio stream bitrate (default "128")
+#   -audio-device string
+#         The PulseAudio source device to record audio from (default "echo-cancel-sink.monitor")
+#   -length int
+#         Length in seconds of replays (default 30)
+#   -o string
+#         Directory to save recordings to (default "/home/deck/Videos/recapture")
+#   -source string
+#         The screen recording source, either "pipewire" or "x11" (default "pipewire")
+#   -tmp string
+#         Directory to save temporary recordings to (only for replay recording) (default "/home/deck/Videos/recapture/.tmp")
+#   -verbose
+#         Print extra debugging info
+
+
+LD_PRELOAD=${LD_PRELOAD/_32/_64}
+
+cd '/home/deck/.var/app/space.crankshaft.Crankshaft/data/crankshaft/plugins/recapture/dist/deps'
+echo $PWD
+
+GST_VAAPI_ALL_DRIVERS=1 \
+GST_PLUGIN_PATH=/home/deck/.var/app/space.crankshaft.Crankshaft/data/crankshaft/plugins/recapture/dist/deps/plugins \
+LD_LIBRARY_PATH=/home/deck/.var/app/space.crankshaft.Crankshaft/data/crankshaft/plugins/recapture/dist/deps/lib \
+./recapture record \
+-audio-device "alsa_output.pci-0000_04_00.5-platform-acp5x_mach.0.HiFi__hw_acp5x_1__sink.monitor" \
+& PIPED_PID=$!
+
+while kill -s 0 $PIPED_PID; do sleep .1; done |
+
+( zenity --progress --window-icon=/usr/share/icons/breeze/actions/16/media-record.svg --width 500 --height 35 --title="Recording in progress" --text='<span font="14" >Screen recording is in progress!\n\nPress <b>Cancel</b> to stop recording.</span>' --pulsate --default-cancel  || kill -2 $PIPED_PID )
+
+
