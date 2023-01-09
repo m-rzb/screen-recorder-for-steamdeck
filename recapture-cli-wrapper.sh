59c71dd5e550b93d7bc6ceb9772af23568bdbe5a
m-rzb <metin@redzheb.me.uk>
2023-01-09 21:21:33 +0000

    automated detection of x11 or wayland

--- recapture-cli-wrapper.sh
+++ recapture-cli-wrapper.sh
@@ -29,19 +29,35 @@
 
 LD_PRELOAD=${LD_PRELOAD/_32/_64}
 
+# Check if current session is using the Wayland Display Server or x11
+echo ${XDG_SESSION_TYPE}
+if [ "${XDG_SESSION_TYPE}" != "wayland" ]; then
+    echo "This session does not use the Wayland Display Server."
+    echo "For screen recording on '${XDG_SESSION_TYPE}' we should use 'x11' as source as well"
+    RECORDING_SOURCE=x11
+else 
+    echo "This session uses the Wayland Display Server."
+    echo "For screen recording on '${XDG_SESSION_TYPE}' we should use 'pipewire' as source"
+    RECORDING_SOURCE=pipewire
+fi
+
+# Check default audio device
+AUDIO_DEVICE=$(pactl get-default-sink)
+echo "Currently detected default audio device is '$AUDIO_DEVICE'." 
+echo "This device will be used to record the audio from."
+
 cd $HOME/.local/recapture
 echo $PWD
 
-AUDIO_DEVICE=$(pactl get-default-sink)
-echo "Currently detected default audio device is '$AUDIO_DEVICE'. This device will be used to record the audio from."
-
+GST_DEBUG=3 \
 GST_VAAPI_ALL_DRIVERS=1 \
 GST_PLUGIN_PATH=$HOME/.local/recapture/plugins \
 LD_LIBRARY_PATH=$HOME/.local/recapture/lib \
 ./recapture record \
+-source $RECORDING_SOURCE \
 -audio-device "$AUDIO_DEVICE.monitor" \
 & PIPED_PID=$!
 
 while kill -s 0 $PIPED_PID; do sleep .1; done |
 
-( zenity --progress --window-icon=/usr/share/icons/breeze/actions/16/media-record.svg --width 500 --height 35 --title="Recording in progress" --text='<span font="14" >Screen recording is in progress!\n\nPress <b>Cancel</b> to stop recording.</span>' --pulsate --default-cancel  || kill -2 $PIPED_PID )
+( zenity --progress --window-icon=/usr/share/icons/breeze/actions/16/media-record.svg --width 555 --height 35 --title="Recording in progress" --text="<big><big><b>Screen recording is in progress!</b></big>\n\nYour system currently uses <b>$XDG_SESSION_TYPE</b> as display sever\nIdentified audio device is <b>$AUDIO_DEVICE</b>\n\nThe video will be saved in <b>$HOME/Videos/recapture/</b>\n\n\nPress <b>Cancel</b> to stop recording.</big>" --pulsate --default-cancel  || kill -2 $PIPED_PID )
