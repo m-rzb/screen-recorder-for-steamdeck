67469e92d2aae14a136d3a28a269f821e53516a1
m-rzb <43208506+m-rzb@users.noreply.github.com>
2023-01-10 23:30:32 +0000

    Update install-recapture-cli.sh

--- install-recapture-cli.sh
+++ install-recapture-cli.sh
@@ -83,7 +83,7 @@ Type=Application' > $HOME/Desktop/Recapture.desktop
 echo "Removing downloaded archives. we do not require them anymore."
 rm -rf /tmp/recapture_dl_dir 
 
-echo "You can now close the terminal window by using CTRL+C or ENTER"
+#echo "You can now close the terminal window by using CTRL+C or ENTER"
 # alternatively
 # exec bash
-read
+#read
