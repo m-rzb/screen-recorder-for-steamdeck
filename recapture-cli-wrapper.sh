094c99c8dac3ce9fa9837dc2346c853f003435a5
m-rzb <43208506+m-rzb@users.noreply.github.com>
2023-01-11 23:00:27 +0000

    Update recapture-cli-wrapper.sh

--- recapture-cli-wrapper.sh
+++ recapture-cli-wrapper.sh
@@ -26,33 +26,33 @@
 #   -verbose
 #         Print extra debugging info
 
-
+# The line below is required only for Steam Deck
 LD_PRELOAD=${LD_PRELOAD/_32/_64}
 
-# Check if current session is using the Wayland Display Server or x11
-# for screen recording to work in Desktop Mode we require libgstximagesrc.so good plugins lib
-#echo ${XDG_SESSION_TYPE}
-#if [ "${XDG_SESSION_TYPE}" != "wayland" ]; then
-#    echo "This session does not use the Wayland Display Server."
-#    echo "For screen recording on '${XDG_SESSION_TYPE}' we should use 'x11' as source as well"
-#    RECORDING_SOURCE=x11
-#else 
-#    echo "This session uses the Wayland Display Server."
-#    echo "For screen recording on '${XDG_SESSION_TYPE}' we should use 'pipewire' as source"
-#    RECORDING_SOURCE=pipewire
-#fi
+cd $HOME/.local/recapture
+echo $PWD
 
-RECORDING_SOURCE=pipewire
+# Check if current session is using the Wayland Display Server or x11
+# echo ${XDG_SESSION_TYPE} # this is not identifying the sessions correctly on Steam Deck
+# Lets try with with loginctl instead..
+LOGIN_SESSION_TYPE=$(loginctl show-session $(loginctl|grep $USER|awk '/'"seat0"'/{print $1}') -p Type | cut -d'=' -f2)
+
+echo $LOGIN_SESSION_TYPE
+if [ "${LOGIN_SESSION_TYPE}" != "wayland" ]; then
+    DISPLAY_SERVER="X11 Display Server"
+    echo "For screen recording on '${DISPLAY_SERVER}' we should use 'x11' plugin"
+    RECORDING_SOURCE=x11
+else 
+    DISPLAY_SERVER="Wayland Display Server"
+    echo "For screen recording on '${DISPLAY_SERVER}' we should use 'pipewire' plugin"
+    RECORDING_SOURCE=pipewire
+fi
 
 # Check default audio device
 AUDIO_DEVICE=$(pactl get-default-sink)
 echo "Currently detected default audio device is '$AUDIO_DEVICE'." 
 echo "This device will be used to record the audio from."
 
-cd $HOME/.local/recapture
-echo $PWD
-
-#GST_DEBUG=3 \
 GST_VAAPI_ALL_DRIVERS=1 \
 GST_PLUGIN_PATH=$HOME/.local/recapture/plugins \
 LD_LIBRARY_PATH=$HOME/.local/recapture/lib \
@@ -63,4 +63,4 @@ LD_LIBRARY_PATH=$HOME/.local/recapture/lib \
 
 while kill -s 0 $PIPED_PID; do sleep .1; done |
 
-( zenity --progress --window-icon=/usr/share/icons/breeze/actions/16/media-record.svg --width 555 --height 35 --title="Recording in progress" --text="<big><big><b>Screen recording is in progress!</b></big>\n\nIdentified audio device is <b>$AUDIO_DEVICE</b>\n\nThe video will be saved in <b>$HOME/Videos/recapture/</b>\n\n\nPress <b>Cancel</b> to stop recording.</big>" --pulsate --default-cancel  || kill -2 $PIPED_PID )
+( zenity --progress --window-icon=/usr/share/icons/breeze/actions/16/media-record.svg --width 555 --height 35 --title="Recording in progress" --text="<big><b>Screen recording is in progress!</b></big>\n\nYour system currently uses <b>$DISPLAY_SERVER</b>.\nIdentified audio device is <b>$AUDIO_DEVICE</b>\n\nThe video will be saved in <b>$HOME/Videos/recapture/</b>\n\n\nPress <b>Cancel</b> to stop recording." --pulsate --default-cancel  || kill -2 $PIPED_PID )
